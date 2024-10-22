7974902c21ced62995b77167f56c9856cf4fe135
Jamey Yadon <jlyadon88@gmail.com>
2024-10-22 08:02:25 -0500

    Removing output files

--- TrackingApp/Project_TrackingApp/config.py
+++
@@ -1,8 +0,0 @@
-userid = '8JRBT0810X181'      # This is the oldest one
-password = '728432OAKI0520X'
-
-# userid2 = '9JRBT08W91093'     # This is the one I had upon registering
-# password2 = 'F8572RF46Q1793S'
-
-# userid3 = '2Y5JRBT080013'     # This is the one I got when I registered
-# password3 = 'Q9936LM60S4052K' # after the fact, as directed.
